3c0356358067d7975f2d4730f00bba92c17a2fc2
Gareth Beddis <garethbeddis@gmail.com>
2019-07-11 01:22:17 +0100

    Added routes, created POST /api/url/shorten

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ connectDB();
 
 app.use(express.json({extended: false}));
 
+// Define routes
+app.use('/', require('./routes/index'));
+app.use('/api/url', require('./routes/url'));
+
+
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
